57f8c867e28415fce9deccaad8856cdb6aeaf930
forest <forestaiste@hotmail.com>
2020-07-21 17:03:24 +0800

    swagger supported profiles

--- src/main/java/com/forest/springbootmybatis/SwaggerConfig.java
+++ src/main/java/com/forest/springbootmybatis/SwaggerConfig.java
@@ -2,22 +2,48 @@ package com.forest.springbootmybatis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
+    public Docket api(Environment env) {
+
+        Profiles profiles = Profiles.of("dev", "test");
+
+        boolean enabled = env.acceptsProfiles(profiles);
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(enabled)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.forest.springbootmybatis.controllers"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("forest aiste", "http://www.sina.com.cn", "aiste@sina.com");
+        return new ApiInfo(
+                "Springboot Mybatis Documentation",
+                "Api Reference Documentation",
+                "1.0",
+                "http://www.sina.com.cn",
+                contact,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList());
     }
 }
